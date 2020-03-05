2a8af332fd0861889cbdb4926b4361a032ba720e
Joseph <jgaiser@hy-vee.com>
2020-03-04 18:29:29 -0600

    spring initializer :fire

---
+++ src/main/kotlin/com/josephgaiser/switchesapi/SwitchesApiApplication.kt
@@ -0,0 +1,11 @@
+package com.josephgaiser.switchesapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SwitchesApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SwitchesApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/josephgaiser/switchesapi/SwitchesApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.josephgaiser.switchesapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SwitchesApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
